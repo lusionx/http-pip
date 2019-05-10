bfe5085f89798878a10cfb7f65e45b8701c13890
liuxing <liuxing@admaster.com.cn>
2019-05-10 16:23:10 +0800

    init k8s

---
+++ build-jwt-od4.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+VERSION=0.0.0
+[ -n "$1" ] && VERSION=$1
+
+sed -i "" "s/1.0.0/${VERSION}/" k8s/package.json
+docker build -f k8s/Dockerfile -t hub.docker.admaster.co/tuice/jwt-od4:${VERSION} .
+git checkout k8s/package.json
+
+docker push hub.docker.admaster.co/tuice/jwt-od4:${VERSION}
